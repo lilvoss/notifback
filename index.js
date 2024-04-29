616a1f430a558a2ee60fbe70d0e6512df59b905a
KHALIL MARZOUKI <80283723+lilvoss@users.noreply.github.com>
2024-04-29 22:50:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { initializeApp, applicationDefault } from 'firebase-admin/app';
+import {initializeApp, applicationDefault } from 'firebase-admin/app';
 import { getMessaging } from "firebase-admin/messaging";
 import express, { json } from "express";
 import cors from "cors";
 import admin from "firebase-admin";
 import serviceAccount from "./serverkey.json" assert { type: "json" };
-import schedule from 'node-schedule';
+
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
@@ -28,47 +28,42 @@ app.use(function(req, res, next) {
   next();
 });
 
+
 initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount),
   projectId: 'notification-c9eb3',
 });
 
-const douaData = {
-  "Monday": "Doua pour le lundi",
-  "Tuesday": "Doua pour le mardi",
-  "Wednesday": "Doua pour le mercredi",
-  "Thursday": "Doua pour le jeudi",
-  "Friday": "Doua pour le vendredi",
-  "Saturday": "Doua pour le samedi",
-  "Sunday": "Doua pour le dimanche"
-};
-
-// Fonction pour envoyer le doua quotidien
-const sendDailyDoua = () => {
-  const today = new Date();
-  const day = today.toLocaleString('en', { weekday: 'long' });
+app.post("/send", function (req, res) {
+  const receivedToken = req.body.fcmToken;
+  
   const message = {
     notification: {
-      title: "Doua du jour",
-      body: douaData[day],
+      title: "Notif",
+      body: 'This is a Test Notification'
     },
-    // Remplacez le token par celui de votre destinataire
     token: "e76EReYaSySl-8KGAdOC6I:APA91bH34YuuwsNnMsOuzHtu-HSGlnPMD3diiTpaXX1Shvlq-BIRKN20DUrSbhl7g8iGErQPiXxEjGuQ7G99F4JzkI1ZtfkQjJ1G2HQVTB2fla4QqtRrr0v4kJD6igD8WqI3YkIlqeWK",
   };
-
-  getMessaging().send(message)
+  
+  getMessaging()
+    .send(message)
     .then((response) => {
-      console.log("Successfully sent daily doua:", response);
+      res.status(200).json({
+        message: "Successfully sent message",
+        token: receivedToken,
+      });
+      console.log("Successfully sent message:", response);
     })
     .catch((error) => {
-      console.error("Error sending daily doua:", error);
+      res.status(400);
+      res.send(error);
+      console.log("Error sending message:", error);
     });
-};
-
-// Planifier l'envoi quotidien du doua à 23h du soir
-schedule.scheduleJob('0 23 * * *', () => {
-  sendDailyDoua();
+  
+  
 });
+
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
